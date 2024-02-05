c3bbd856921d313992c0ff926ef1a609b8864a88
Drew0004 <drdrewgraphic@gmail.com>
2024-02-05 15:31:16 +0100

    fix style

--- index.php
+++ index.php
@@ -4,25 +4,37 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Home Page Form</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     </head>
-<body>
-    <h1>PHP Badwords</h1>
-    <form action="./usertext.php" method="GET">
-        
-        <label for="user-text">
-            Inserisci il tuo paragrafo
-        </label>
-        <input id="user-text" type="textarea" name="paragraph" placeholder="Inserisci un paragrafo..">
-        
-        <label for="user-text">
-            Inserisci la parola da censurare
-        </label>
-        <input id="user-text" type="text" name="badword" placeholder="Inserisci la badword..">
-        
-        <button type="submit">
-            Vai all'altra pagina
-        </button>
-    
-    </form>
+<body class="bg-dark">
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <h1 class="text-white text-center my-4">PHP Badwords</h1>
+            <div class="col-6">
+                <form action="./usertext.php" method="GET">
+                    <div>
+                        <label class="text-white mb-2" for="user-text">
+                            Inserisci il tuo paragrafo
+                        </label>
+                    </div>
+                        <input class="mb-4 w-100 form-control" id="user-text" type="textarea" name="paragraph" placeholder="Inserisci un paragrafo.." style="height: 100px">
+                    <div>
+                        <label class="text-white mb-2" for="user-text">
+                            Inserisci la parola da censurare
+                        </label>
+                    </div>
+                        <input class="mb-4 w-100 form-control" id="user-text" type="text" name="badword" placeholder="Inserisci la badword..">
+                    <div class="text-center">
+                        <button class="btn btn-primary" type="submit">
+                            Vai all'altra pagina
+                        </button>
+                    </div>
+                
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
--- usertext.php
+++ usertext.php
@@ -11,28 +11,36 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>User texts</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     </head>
-<body>
-    <h1>Area testo utente</h1>
-    
-    <p><strong> Il paragrafo iniziale è: </strong> 
-        <?php echo $paragraph ?> 
-    </p>
-    
-    <h5>La lunghezza del paragrafo è 
-        <?php echo strlen($paragraph) ?> 
-    </h5>
-    
-    <h3> La parola da censurare è: 
-        <?php echo $badword ?> 
-    </h3>
-    
-    <p><strong>Il paragrafo censurato è: </strong> 
-        <?php echo $censoredParagraph ?> 
-    </p>
-
-    <h5>La lunghezza del paragrafo è 
-        <?php echo strlen($censoredParagraph) ?> 
-    </h5>
+<body class="bg-dark">
+    <div class="container">
+        <div class="border rounded-4 p-5 my-5">
+            <h1 class="text-center text-white mb-5 fw-bold">Area testo utente</h1>
+            
+            <p class="text-white"><strong> Il paragrafo iniziale è: </strong> 
+                <?php echo $paragraph ?> 
+            </p>
+            
+            <h5 class="text-center text-white my-3">La lunghezza del paragrafo è:
+                <?php echo strlen($paragraph) ?> 
+            </h5>
+        </div>
+        
+        <div class="border rounded-4 p-5 my-5">
+            <h3 class="text-white text-center mb-5 fw-bold"> La parola da censurare è: 
+                <?php echo $badword ?> 
+            </h3>
+            
+            <p class="text-white"><strong>Il paragrafo censurato è: </strong> 
+                <?php echo $censoredParagraph ?> 
+            </p>
+        
+            <h5 class="text-center text-white my-3">La lunghezza del paragrafo è:
+                <?php echo strlen($censoredParagraph) ?> 
+            </h5>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
